d1be0c6a59db6f7e5781bc366be091e52f9bacbf
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-13 13:04:50 -0700

    Create reverseString function that passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let letters = text.split('');
+    let textLength = text.length;
+    const reverseLetters = [];
+    
+    for (let i=0; i<textLength; i++) {
+        reverseLetters[i] = letters[textLength-i-1];
+    }
 
+    reverseText = reverseLetters.join('');
+    return reverseText;
 };
 
 // Do not edit below this line
