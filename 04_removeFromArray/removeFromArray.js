2e509369edf170958af7c3b9ab8dc6fee7c9d1f6
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-14 09:58:16 -0700

    Create removeFromArray function that passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removeItemsArray) { 
+    const outputArray = inputArray;
+    const inputArrayLength = inputArray.length;
+    const removeItemsArrayLength = removeItemsArray.length;
 
+    for (let i=0; i<inputArrayLength; i++) {
+        for (let j=0; j<removeItemsArrayLength; j++) {
+            if (inputArray.includes(removeItemsArray[j])) {
+                let index = outputArray.indexOf(removeItemsArray[j]);
+                outputArray.splice(index, 1);
+            }
+        }
+    }
+    
+    return outputArray;
 };
 
 // Do not edit below this line
