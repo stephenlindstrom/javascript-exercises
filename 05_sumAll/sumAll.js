3af107b6322b88fc577854efe5c0b21e5e51e511
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-15 12:04:45 -0700

    Create sum function that passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    let sum = 0;
+    let startValue;
+    let endValue;
+    
+    if (value1 > value2) {
+        startValue = value2;
+        endValue = value1;
+    }
+    else {
+        startValue = value1;
+        endValue = value2;
+    }
 
+    if (value1 < 0 || value2 < 0) {
+        return 'ERROR';
+    }
+
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+        return 'ERROR';
+    }
+
+    for (let i=startValue; i <= endValue; i++) {
+        sum = sum + i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
