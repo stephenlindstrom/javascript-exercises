1536ef97d8035482e99c47decb64de7fcdfcd9df
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-15 12:26:07 -0700

    Create functions that convert temps between C and F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  const tempInCelsius = Math.round((tempInFahrenheit - 32) * 5/9 * 10)/ 10; 
+  return tempInCelsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const tempInFahrenheit = Math.round((tempInCelsius * 9/5 + 32)* 10)/ 10;
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
