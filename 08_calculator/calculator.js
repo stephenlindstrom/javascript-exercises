f87371e868d18cb3b67eba876696580a65bff33e
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-16 09:56:07 -0700

    Create calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(int1, int2) {
+  return int1 + int2;
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+  return int1 - int2;
 };
 
-const sum = function() {
-	
+const sum = function(intArray) {
+	let sum = 0;
+  for (let int of intArray) {
+    sum += int;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(intArray) {
+  if (!intArray.length) {
+    return 'ERROR';
+  }
 
+  let product = 1;
+  for (let int of intArray) {
+    product *= int;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let power = 1;
+  for (let i=0; i<exponent; i++) {
+    power *= base;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+	let factorial = 1;
+  for (let i=int; i>1; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
