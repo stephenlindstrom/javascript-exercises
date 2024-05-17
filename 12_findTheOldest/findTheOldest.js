612f319b2180fe4f54e847364578e9dfb615afa1
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-17 12:20:54 -0700

    Create function that finds oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson;
 
+    for (const person of people) {
+        if (person.yearOfDeath == null) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
