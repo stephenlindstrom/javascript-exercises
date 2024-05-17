a049ee10c3a9a35ad30ea76924ad2b735e313e5c
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-17 11:20:15 -0700

    Create function that returns Fibonacci value

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let leftPointer = 1;
+    let rightPointer = 1;
 
+    if (index == 0) {
+        return 0;
+    }
+
+    if (index < 0) {
+        return 'OOPS';
+    }
+
+    for (let i=2; i<index; i++) {
+        let sum = leftPointer + rightPointer;
+        leftPointer = rightPointer;
+        rightPointer = sum;
+    }
+
+    return rightPointer;
 };
 
 // Do not edit below this line
