3e4aa9f2a11353a3eea314cfe79240f3b035f5c1
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-17 10:51:22 -0700

    Create function that successfully tests for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwyxz0123456789';
 
+    function alphaNumeric(value) {
+        return alphanumerical.includes(value);
+    }
+
+    const stringAlphaNum = string.toLowerCase().split('').filter(alphaNumeric).join('');
+
+    const reverseStringAlphaNum = stringAlphaNum.split('').reverse().join('')
+
+    if (stringAlphaNum === reverseStringAlphaNum) {
+        return true;
+    } 
+
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
